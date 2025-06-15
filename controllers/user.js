e484c71f1f6e245d43d3d1e19966f91ecab3fafa
sasindubm <msasindu450@gmail.com>
2025-06-15 14:23:07 +0530

    updated

--- controllers/user.js
+++ controllers/user.js
@@ -25,3 +25,55 @@ exports.getAssesment = (req, res) => {
 exports.getPage1 = (req, res) => {
     res.render('page1', { title: 'Page1', navigation: false });
 };
+
+exports.getPage2 = (req, res) => {
+    res.render('page2', { title: 'Page2', navigation: false });
+};
+
+exports.getPage3 = (req, res) => {
+    res.render('page3', { title: 'Page3', navigation: false });
+};
+
+exports.getPage4 = (req, res) => {
+    res.render('page4', { title: 'Page4', navigation: false });
+};
+
+exports.getPage5 = (req, res) => {
+    res.render('page5', { title: 'Page5', navigation: false });
+};
+
+exports.getPage6 = (req, res) => {
+    res.render('page6', { title: 'Page6', navigation: false });
+};
+
+exports.getPage7 = (req, res) => {
+    res.render('page7', { title: 'Page7', navigation: false });
+};
+
+exports.getPage8 = (req, res) => {
+    res.render('page8', { title: 'Page8', navigation: false });
+};
+
+exports.getPage9 = (req, res) => {
+    res.render('page9', { title: 'Page9', navigation: false });
+};
+
+exports.getPage10 = (req, res) => {
+    res.render('page10', { title: 'Page10', navigation: false });
+};
+
+exports.getPage11 = (req, res) => {
+    res.render('page11', { title: 'Page11', navigation: false });
+};
+
+exports.getPage12 = (req, res) => {
+    res.render('page12', { title: 'Page12', navigation: false });
+};
+
+exports.getPage13 = (req, res) => {
+    res.render('page13', { title: 'Page13', navigation: false });
+};
+
+exports.getPage14 = (req, res) => {
+    res.render('page14', { title: 'Page14', navigation: false });
+};
--- routes/routes.js
+++ routes/routes.js
@@ -19,4 +19,30 @@ router.get('/assesment', userController.getAssesment);
 
 router.get('/page1', userController.getPage1);
 
+router.get('/page2', userController.getPage2);
+
+router.get('/page3', userController.getPage3);
+
+router.get('/page4', userController.getPage4);
+
+router.get('/page5', userController.getPage5);
+
+router.get('/page6', userController.getPage6);
+
+router.get('/page7', userController.getPage7);
+
+router.get('/page8', userController.getPage8);
+
+router.get('/page9', userController.getPage9);
+
+router.get('/page10', userController.getPage10);
+
+router.get('/page11', userController.getPage11);
+
+router.get('/page12', userController.getPage12);
+
+router.get('/page13', userController.getPage13);
+
+router.get('/page14', userController.getPage14);
+
 module.exports = router;
