6b2f95e88b5c73fd646ce734561311de8e604d97
sasindubm <msasindu450@gmail.com>
2025-11-15 21:58:17 +0530

    remote db integration

--- config/db.js
+++ config/db.js
@@ -1,8 +1,11 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
+const dbPort = process.env.DB_PORT;
+
 const conn = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: dbPort,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
